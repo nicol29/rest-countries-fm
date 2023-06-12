4d1ecdbf6f75067494c196063d944be264453cc3
nicol29 <nicolascondrea@gmail.com>
2023-06-12 19:14:35 +0100

    Implement country search and filtering on homepage

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,4 +1,6 @@
 import "../styles/light/countryCard.scss";
+import { useNavigate } from "react-router";
+
 
 interface CountryInterface {
   country: any;
@@ -6,12 +8,23 @@ interface CountryInterface {
 
 
 const CountryCard : React.FC<CountryInterface> = ({ country }) => {
+  const navigate = useNavigate();
+
+  const formatNumberWithCommas = () => {
+    return country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  const navigateToDetailedDesc = () => {
+    navigate(`/countries/${country.name.common}`);
+    navigate(0);
+  }
+
   return (
-    <div className="country-card">
-      <img src={country.flags.png}/>
+    <div className="country-card" onClick={navigateToDetailedDesc}>
+      {/* <img src={country.flags.png}/> */}
       <div>
         <h2>{country.name.common}</h2>
-        <p><span>Population: </span>{country.population}</p>
+        <p><span>Population: </span>{formatNumberWithCommas()}</p>
         <p><span>Region: </span>{country.region}</p>
         <p><span>Capital: </span>{country.capital}</p>
       </div>
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,25 +1,52 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import ThemeToggler from "./ThemeToggler";
 import CountryCard from "./CountryCard";
 import searchLight from "../assets/images/icons/light/magnify-custom.png";
 import chevronLight from "../assets/images/icons/light/chevron-down-custom.png";
 import uniqid from 'uniqid';
-import { Routes, Route } from 'react-router-dom';
 
 
 const HomePage = () => {
   const [countries, setCountries] = useState([]);
+  const [filterToggle, setFilterToggle] = useState(false);
+  const [input, setInput] = useState("");
+  const dropDownOptions = useRef<HTMLDivElement | null>(null);
 
+  const searchForFilter = async (continent: string) => {
+    const response = await fetch(`https://restcountries.com/v3.1/region/${continent}`);
+    const data = await response.json();
 
-  useState(() => {
+    setCountries(data);
+  }
+
+  const searchForCountry = async () => {
+    const res = await fetch(`https://restcountries.com/v3.1/name/${input}`);
+    const data = await res.json();
+
+    setCountries(data);
+  }
+
+  const manageInput = (e : React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      searchForCountry();
+      setInput("");
+    }
+  }
+
+  useEffect(() => {
     (async () => {
       const response = await fetch("https://restcountries.com/v3.1/all");
       const data = await response.json();
 
-      console.log(data)
       setCountries(data);
     })()
   }, []);
+
+  useEffect(() => {
+    if (dropDownOptions.current) {
+      filterToggle ? dropDownOptions.current.style.display = "block" : dropDownOptions.current.style.display = "none";
+    }
+  }, [filterToggle]);
 
   return (
     <>
@@ -28,29 +55,38 @@ const HomePage = () => {
         <ThemeToggler />
       </header>
       <section>
-        <div className="search-filter">
-          <div className="search-bar">
-            <img src={searchLight}/>
-            <input type="text" placeholder="Search for a country..."/>
-          </div>
-          <div className="dropdown-menu">
-            <p>Filter by Region</p>
-            <img src={chevronLight}/>
-            <div className="options">
-              <ul>
-                <li>Africa</li>
-                <li>America</li>
-                <li>Asia</li>
-                <li>Europe</li>
-                <li>Oceania</li>
-              </ul>
+        <div className="max-width">
+          <div className="search-filter">
+            <div className="search-bar">
+              <img src={searchLight} onClick={() => searchForCountry()}/>
+              <input 
+                type="text" 
+                placeholder="Search for a country..." 
+                value={input} 
+                onChange={(e) => 
+                setInput(e.target.value)} 
+                onKeyDown={(e) => manageInput(e)}
+              />
+            </div>
+            <div className="dropdown-menu" onClick={() => setFilterToggle(!filterToggle)}>
+              <p>Filter by Region</p>
+              <img src={chevronLight}/>
+              <div className="options" ref={dropDownOptions}>
+                <ul>
+                  <li onClick={() => searchForFilter("Africa")}>Africa</li>
+                  <li onClick={() => searchForFilter("America")}>America</li>
+                  <li onClick={() => searchForFilter("Asia")}>Asia</li>
+                  <li onClick={() => searchForFilter("Europe")}>Europe</li>
+                  <li onClick={() => searchForFilter("Oceania")}>Oceania</li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="country-cards-container">
-          {countries.map(country => (
-            <CountryCard country={country} key={uniqid()}/>
-          ))}
+          <div className="country-cards-container">
+            {countries.map(country => (
+              <CountryCard country={country} key={uniqid()}/>
+            ))}
+          </div>
         </div>
       </section>
     </>
